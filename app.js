7bd26ac8194b2189498254c00f1c2bb0457ba41e
Ankit Kumar Sharma <ankit.sharma@qontenttai.com>
2024-08-11 02:44:38 +0530

    completed: models

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log("Hello from app.js");
+import express from "express";
+import userRoute from "./routes/user.js";
+
+const app = express();
+
+app.use("/user", userRoute);
+
+app.get("/", (req, res) => {
+  res.send("Welcome to ChatMitra!!");
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
